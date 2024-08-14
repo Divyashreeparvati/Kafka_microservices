cedffae721e4b0493a3281afcc068a8f037d301a
Divyashreeparvati <divibs01@gmail.com>
2024-08-14 12:29:03 +0530

    jest

--- jest.config.ts
+++ jest.config.ts
@@ -6,16 +6,7 @@
 import type {Config} from 'jest';
 
 const config: Config = {
-  // All imported modules in your tests should be mocked automatically
-  // automock: false,
-
-  // Stop running tests after `n` failures
-  // bail: 0,
-
-  // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "C:\\Users\\prana\\AppData\\Local\\Temp\\jest",
-
-  // Automatically clear mock calls, instances, contexts and results before every test
+  preset:"ts-jest",
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
